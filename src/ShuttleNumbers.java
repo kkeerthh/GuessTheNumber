f8911d26a14fc94ce1ca8c099c764014297812a6
Kira Hovorukha <skitershovor@gmail.com>
2024-03-11 09:23:21 +0200

    second commit for ShuttleNumbers

--- src/ShuttleNumbers.java
+++ src/ShuttleNumbers.java
@@ -1,21 +1,33 @@
-public class ShuttleNumbers {
+import java.util.Random;
 
+public class ShuttleNumbers {
     public static void main(String[] args) {
         int[] arr = new int[100];
 
         for (int i = 0, j = 0; i < arr.length; i++) {
-                if (i + 1 != 4 && i + 1 != 9) {
-                    arr[j++] = i + 1;
+            int numberOfShuttle = i + 1;
+            if (numberOfShuttle % 10 != 4 && numberOfShuttle % 10 != 9 && numberOfShuttle / 10 % 10 != 4 && numberOfShuttle / 10 % 10 != 9) {
+                arr[j++] = numberOfShuttle;
+            } else {
+                arr[j++] = -1;
+            }
+
+        }
+
+        for (int array : arr) {
+            if (array != 0) {
+                if (array != -1) {
+                    System.out.println("Номер шатла: " + array);
+                } else {
+                    int randomNum;
+                    do {
+                        randomNum = new Random().nextInt(100 - 1) + 1;
+                    } while (randomNum % 10 == 4 || randomNum % 10 == 9 || randomNum / 10 % 10 == 4 || randomNum / 10 % 10 == 9);
+                    System.out.println("Номер шатла: " + randomNum);
+                }
             }
         }
-
-        int[] newArr = new int[arr.length-2];
-        System.arraycopy(arr, 0, newArr, 0, newArr.length);
-
-            for (int array : newArr) {
-
-                System.out.println("Номер шатла: " + array);
-        }
-
     }
 }
+
+
