6947b86fdfcc5e41d147bc9884369b1ca1f16bc6
Kira Hovorukha <skitershovor@gmail.com>
2024-03-11 10:30:36 +0200

    third commit for ShuttleNumbers

--- src/ShuttleNumbers.java
+++ src/ShuttleNumbers.java
@@ -1,33 +1,23 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class ShuttleNumbers {
     public static void main(String[] args) {
-        int[] arr = new int[100];
+            int[] arr = new int[100];
+            int numberOfShuttle = 1;
 
-        for (int i = 0, j = 0; i < arr.length; i++) {
-            int numberOfShuttle = i + 1;
-            if (numberOfShuttle % 10 != 4 && numberOfShuttle % 10 != 9 && numberOfShuttle / 10 % 10 != 4 && numberOfShuttle / 10 % 10 != 9) {
-                arr[j++] = numberOfShuttle;
-            } else {
-                arr[j++] = -1;
-            }
-
-        }
-
-        for (int array : arr) {
-            if (array != 0) {
-                if (array != -1) {
-                    System.out.println("Номер шатла: " + array);
+            for (int i = 0; i < arr.length; i++) {
+                if (numberOfShuttle % 10 != 4 && numberOfShuttle % 10 != 9 && numberOfShuttle / 10 % 10 != 4 && numberOfShuttle / 10 % 10 != 9) {
+                    arr[i] = numberOfShuttle;
+                    numberOfShuttle++;
                 } else {
-                    int randomNum;
-                    do {
-                        randomNum = new Random().nextInt(100 - 1) + 1;
-                    } while (randomNum % 10 == 4 || randomNum % 10 == 9 || randomNum / 10 % 10 == 4 || randomNum / 10 % 10 == 9);
-                    System.out.println("Номер шатла: " + randomNum);
+                    numberOfShuttle++;
+                    i--;
                 }
             }
+
+            System.out.println("Номери шатлів: " + Arrays.toString(arr));
         }
-    }
 }
 
 
